f8f974c68437a258ebc3f3181ee8f14f531e332e
Sebastians8448 <83966536+Sebastians8448@users.noreply.github.com>
2021-06-18 09:32:26 -0500

    Add files via upload

---
+++ plantilla/index.js
@@ -0,0 +1,87 @@
+//console.log('hola mundo');
+//console.log('5+3');
+//console.log(5+3);
+//let nombre = 'a';
+//console.log(nombre);
+//let evaluador = true;
+//console.log(evaluador)
+//let vacio =null;
+//console.log(vacio);
+//let numero = 1;
+//let numero1 =1.5;
+//console.log(numero+numero1);
+//const miObjeto = {
+//    nombre : 'sebastians',
+//    apellido : 'galindez',
+//}
+//let numero2 = '5';
+//console.log(miObjeto.nombre+miObjeto.apellido,numero2);
+//numero2 = numero2 + numero
+//console.log(numero2);
+//numero2 = (20);
+//console.log(numero2);
+
+//let arreglo = []; //array
+//console.log(arreglo);
+//arreglo.push(numero2);
+//console.log(arreglo);
+//for (let i = 0; i < 6; i++) {
+//    arreglo.push(prompt('escribe tu libro favorito: '));
+//    console.log(arreglo[i]);
+//}
+//console.log(arreglo);
+
+//let arr = [1, 2, 3, 4, 5];
+//console.log(arr);
+//arr.forEach(element => {
+//    element = element * 8 - 2;
+//    console.log(element);
+//});
+
+//let num = parseInt(prompt('escribe un numero: '));
+//num = num * 2;
+//console.log(num);
+//num *=2;
+//console.log(num);}
+
+//if (num==24) {
+//    alert('aun te falta');
+//}
+//else{
+//    alert('no aplicas');
+//}
+//if(num >= 35 || num <= 100) {
+//    alert('ahi vamos')
+//    if (num < 50) {
+//        alert('estas a tiempo');
+//    }
+//} else {
+//    alert('te pasaste');
+//}
+
+switch (num) {
+    case 1:
+        alert('ve al salon 24')
+        break;
+    case 2:
+        alert('dirigete a tesoreria')
+        break;
+    case 3:
+        alert('quedate en casa')
+        break;
+    
+        default:
+            alert('ve a informacion')
+            break;
+}
+
+let n = 0;
+while (n<10) {
+    alert ('te sobran '+n+' dolares');
+    index++
+}
+for (let index 0 0; index < arr.length; index++) {
+    const element = arr[index];
+    console.log(element,index);
+}
+console.log(arr.length);
